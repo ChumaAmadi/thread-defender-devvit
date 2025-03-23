21e1d00ac4a9698a9e192a0d81aa31c1098fbf2c
chuma <C.NnodimAmadi001@umb.edu>
2025-03-22 22:08:31 -0500

    Downgraded To Tailwind v3

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.join(__dirname, "game"), // Point to your app directory
+  root: path.join(__dirname, "game"),
   build: {
-    outDir: path.join(__dirname, "webroot"), // Specify your desired output directory
-    emptyOutDir: true, // Clean the output directory before each build
-    copyPublicDir: true, // Copies over assets
-    sourcemap: true, // Enable sourcemaps
+    outDir: path.join(__dirname, "webroot"),
+    emptyOutDir: true,
+    copyPublicDir: true,
+    sourcemap: true,
   },
+  css: {
+    postcss: './postcss.config.js',
+  }
 });
